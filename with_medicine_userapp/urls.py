2562c83d42db0efcd95a95a6a5321f53f8c54f16
CMH <skdaks5701@naver.com>
2023-08-14 01:04:40 +0900

    user_detail/login_form/signup_form/main.nav edit

--- with_medicine_userapp/urls.py
+++ with_medicine_userapp/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('user_update/', views.user_update, name='user_update'),
     path('user_delete/', views.user_delete, name='user_delete'),
     path('password/', views.change_password, name='change_password'),
-    path('user_detail', views.user_detail, name='user_detail'), 
+     
     path('signup_welcome/', views.signup_welcome, name='signup_welcome'),
 ]
