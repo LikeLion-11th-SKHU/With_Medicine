2fc8b3c9f332fe1805ec49c15851f61cfdf36b53
CMH <skdaks5701@naver.com>
2023-08-18 17:36:50 +0900

    fix : all delete

--- with_medicine_free/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def free_read(request):
-    return render(request, 'free_read.html')
--- with_medicine_review/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def review_read(request):
-    return render(request, 'review_read.html')
